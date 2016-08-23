a135fd35665984d69126aad585b694871023be37
douhaoqiang <douhaoqiang@126.com>
2016-08-23 17:37:54 +0800

    qqq

--- app/src/main/java/com/dhq/demo/base/BaseActivity.java
+++ app/src/main/java/com/dhq/demo/base/BaseActivity.java
@@ -1,7 +1,13 @@
 package com.dhq.demo.base;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import butterknife.ButterKnife;
@@ -12,14 +18,61 @@ import butterknife.ButterKnife;
 public abstract class BaseActivity<V, P extends BasePresenter<V>> extends Activity {
 
     protected P mPresenter;
+    private static final int INVALID_VAL = -1;
+
+    private static final int COLOR_DEFAULT = Color.parseColor("#FA7198");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
         setContentView(getLayoutId());
         ButterKnife.bind(this);
         mPresenter = createPresenter();
         mPresenter.attachView((V) this);
+    }
+
+
+    private void setStatusBar(int statusColor){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (statusColor != INVALID_VAL) {
+                getWindow().setStatusBarColor(statusColor);
+            }
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            int color = COLOR_DEFAULT;
+            ViewGroup contentView = (ViewGroup) findViewById(android.R.id.content);
+            if (statusColor != INVALID_VAL) {
+                color = statusColor;
+            }
+            View statusBarView = contentView.getChildAt(0);
+            // 改变颜色时避免重复添加statusBarView
+            if (statusBarView != null && statusBarView.getMeasuredHeight() == getStatusBarHeight(this)) {
+                statusBarView.setBackgroundColor(color);
+                return;
+            }
+            statusBarView = new View(this);
+            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getStatusBarHeight(this));
+            statusBarView.setBackgroundColor(color);
+            contentView.addView(statusBarView, lp);
+        }
+    }
+
+    private int getStatusBarHeight(Context context) {
+
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 
 
