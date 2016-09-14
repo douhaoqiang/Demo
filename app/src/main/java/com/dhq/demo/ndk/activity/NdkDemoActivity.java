d8ebe0c9dc895c9dd8d3431bc8358ce31116b255
douhaoqiang <douhaoqiang@126.com>
2016-09-14 11:14:52 +0800

    ss

--- app/src/main/java/com/dhq/demo/ndk/activity/NdkDemoActivity.java
+++ app/src/main/java/com/dhq/demo/ndk/activity/NdkDemoActivity.java
@@ -7,8 +7,15 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
+import android.text.style.AbsoluteSizeSpan;
 import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StrikethroughSpan;
+import android.text.style.StyleSpan;
+import android.text.style.SubscriptSpan;
+import android.text.style.SuperscriptSpan;
+import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -50,8 +57,7 @@ public class NdkDemoActivity extends Activity {
             public void updateDrawState(TextPaint ds) {
                 super.updateDrawState(ds);
                 ds.setColor(Color.WHITE);       //设置文件颜色
-                ds.setUnderlineText(true);      //设置下划线
-
+//                ds.setUnderlineText(false);      //设置下划线
             }
 
             @Override
@@ -67,7 +73,7 @@ public class NdkDemoActivity extends Activity {
             public void updateDrawState(TextPaint ds) {
                 super.updateDrawState(ds);
                 ds.setColor(Color.WHITE);       //设置文件颜色
-                ds.setUnderlineText(true);      //设置下划线
+//                ds.setUnderlineText(false);      //设置下划线
             }
 
             @Override
@@ -76,7 +82,38 @@ public class NdkDemoActivity extends Activity {
             }
         }, 9, url_0_text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
-        spStr.setSpan(new ForegroundColorSpan(Color.RED), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);  //设置字体颜色为红色
+
+        //设置字体大小（绝对值,单位：像素）
+        spStr.setSpan(new AbsoluteSizeSpan(20), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        //第二个参数boolean dip，如果为true，表示前面的字体大小单位为dip，否则为像素，同上。
+        spStr.setSpan(new AbsoluteSizeSpan(20,true), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+
+        //设置字体大小（相对值,单位：像素） 参数表示为默认字体大小的多少倍
+        spStr.setSpan(new RelativeSizeSpan(0.5f), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);  //0.5f表示默认字体大小的一半
+        spStr.setSpan(new RelativeSizeSpan(2.0f), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);  //2.0f表示默认字体大小的两倍
+
+
+        //设置字体样式正常，粗体，斜体，粗斜体
+        spStr.setSpan(new StyleSpan(android.graphics.Typeface.NORMAL), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);  //正常
+        spStr.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);  //粗体
+        spStr.setSpan(new StyleSpan(android.graphics.Typeface.ITALIC), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);  //斜体
+        spStr.setSpan(new StyleSpan(android.graphics.Typeface.BOLD_ITALIC), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);  //粗斜体
+
+        //设置下划线
+//        spStr.setSpan(new UnderlineSpan(), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        //设置删除线
+//        spStr.setSpan(new StrikethroughSpan(), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        //设置下标
+//        spStr.setSpan(new SubscriptSpan(), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        //设置上标
+//        spStr.setSpan(new SuperscriptSpan(), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+
+//        spStr.setSpan(new ForegroundColorSpan(Color.RED), 0, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);  //设置字体颜色为红色
+
         text_tv.setHighlightColor(Color.TRANSPARENT); //设置点击后的颜色为透明，否则会一直出现高亮
         text_tv.setText(spStr);
         text_tv.setMovementMethod(LinkMovementMethod.getInstance());//开始响应点击事件
