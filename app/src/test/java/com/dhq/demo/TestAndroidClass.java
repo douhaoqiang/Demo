28a5b64e97d14f2208af05959b8f8c08de585158
douhaoqiang <douhaoqiang@126.com>
2016-09-18 15:15:19 +0800

    ss

---
+++ app/src/test/java/com/dhq/demo/TestAndroidClass.java
@@ -0,0 +1,18 @@
+package com.dhq.demo;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * DESC
+ * Created by douhaoqiang on 2016/9/14.
+ */
+
+public class TestAndroidClass extends InstrumentationTestCase {
+
+
+    private static final String TAG = "TestAndroidClass";
+
+    public void testRes() throws Exception{
+        launchActivity("com.dhq.demo",MainActivity.class,null);
+    }
+}
