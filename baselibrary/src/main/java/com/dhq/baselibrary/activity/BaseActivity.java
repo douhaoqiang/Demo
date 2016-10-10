7950568913b533764a84158cba90486843d85227
douhaoqiang <douhaoqiang@126.com>
2016-10-10 17:16:16 +0800

    6.0权限

--- baselibrary/src/main/java/com/dhq/baselibrary/activity/BaseActivity.java
+++ baselibrary/src/main/java/com/dhq/baselibrary/activity/BaseActivity.java
@@ -7,11 +7,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.dhq.baselibrary.R;
 import com.dhq.baselibrary.presenter.BasePresenter;
+import com.dhq.baselibrary.util.PermissionUtils;
 
 /**
  * DESC
@@ -47,7 +47,7 @@ public abstract class BaseActivity<V, P extends BasePresenter<V>> extends AppCom
     }
 
 
-    private void setStatusBar(int statusColor){
+    private void setStatusBar(int statusColor) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             if (statusColor != INVALID_VAL) {
                 getWindow().setStatusBarColor(statusColor);
@@ -119,18 +119,105 @@ public abstract class BaseActivity<V, P extends BasePresenter<V>> extends AppCom
 
     /**
      * 显示Toast消息
+     *
      * @param msg 需要显示的消息
      */
-    public void showToast(String msg){
-        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    public void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
     /**
      * 显示Toast消息
+     *
      * @param resId 需要显示的消息id
      */
-    public void showToast(int resId){
-        Toast.makeText(this,resId,Toast.LENGTH_SHORT).show();
+    public void showToast(int resId) {
+        Toast.makeText(this, resId, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     *
+     */
+    public void showCamera() {
+        PermissionUtils.requestPermission(this, PermissionUtils.CODE_CAMERA, mPermissionGrant);
+    }
+
+    public void getAccounts() {
+        PermissionUtils.requestPermission(this, PermissionUtils.CODE_GET_ACCOUNTS, mPermissionGrant);
+    }
+
+    public void callPhone() {
+        PermissionUtils.requestPermission(this, PermissionUtils.CODE_CALL_PHONE, mPermissionGrant);
+    }
+
+    public void readPhoneState() {
+        PermissionUtils.requestPermission(this, PermissionUtils.CODE_READ_PHONE_STATE, mPermissionGrant);
+    }
+
+    public void accessFineLocation() {
+        PermissionUtils.requestPermission(this, PermissionUtils.CODE_ACCESS_FINE_LOCATION, mPermissionGrant);
+    }
+
+    public void accessCoarseLocation() {
+        PermissionUtils.requestPermission(this, PermissionUtils.CODE_ACCESS_COARSE_LOCATION, mPermissionGrant);
+    }
+
+    public void readExternalStorage() {
+        PermissionUtils.requestPermission(this, PermissionUtils.CODE_READ_EXTERNAL_STORAGE, mPermissionGrant);
+    }
+
+    public void writeExternalStorage() {
+        PermissionUtils.requestPermission(this, PermissionUtils.CODE_WRITE_EXTERNAL_STORAGE, mPermissionGrant);
+    }
+
+    public void recordAudio() {
+        PermissionUtils.requestPermission(this, PermissionUtils.CODE_RECORD_AUDIO, mPermissionGrant);
+    }
+
+
+    private PermissionUtils.PermissionGrant mPermissionGrant = new PermissionUtils.PermissionGrant() {
+        @Override
+        public void onPermissionGranted(int requestCode) {
+            switch (requestCode) {
+                case PermissionUtils.CODE_RECORD_AUDIO:
+                    showToast("Result Permission Grant CODE_RECORD_AUDIO");
+                    break;
+                case PermissionUtils.CODE_GET_ACCOUNTS:
+                    showToast("Result Permission Grant CODE_GET_ACCOUNTS");
+                    break;
+                case PermissionUtils.CODE_READ_PHONE_STATE:
+                    showToast("Result Permission Grant CODE_READ_PHONE_STATE");
+                    break;
+                case PermissionUtils.CODE_CALL_PHONE:
+                    showToast("Result Permission Grant CODE_CALL_PHONE");
+                    break;
+                case PermissionUtils.CODE_CAMERA:
+                    showToast("Result Permission Grant CODE_CAMERA");
+                    break;
+                case PermissionUtils.CODE_ACCESS_FINE_LOCATION:
+                    showToast("Result Permission Grant CODE_ACCESS_FINE_LOCATION");
+                    break;
+                case PermissionUtils.CODE_ACCESS_COARSE_LOCATION:
+                    showToast("Result Permission Grant CODE_ACCESS_COARSE_LOCATION");
+                    break;
+                case PermissionUtils.CODE_READ_EXTERNAL_STORAGE:
+                    showToast("Result Permission Grant CODE_READ_EXTERNAL_STORAGE");
+                    break;
+                case PermissionUtils.CODE_WRITE_EXTERNAL_STORAGE:
+                    showToast("Result Permission Grant CODE_WRITE_EXTERNAL_STORAGE");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, String[] permissions, int[] grantResults) {
+        PermissionUtils.requestPermissionsResult(this, requestCode, permissions, grantResults, mPermissionGrant);
     }
 
 
