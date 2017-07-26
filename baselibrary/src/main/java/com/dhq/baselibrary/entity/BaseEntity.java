e50982abb527034d3ffc6c395c4d1ff1c7547e65
douhaoqiang <douhaoqiang@126.com>
2017-07-26 10:33:45 +0800

    实体基类

---
+++ baselibrary/src/main/java/com/dhq/baselibrary/entity/BaseEntity.java
@@ -0,0 +1,11 @@
+package com.dhq.baselibrary.entity;
+
+import java.io.Serializable;
+
+/**
+ * DESC 实体类 基类
+ * Created by douhaoqiang on 2017/7/26.
+ */
+
+public class BaseEntity implements Serializable {
+}
