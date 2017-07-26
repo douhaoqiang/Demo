8921913ae158f7f32cfd32aad0121351fa63e8e8
douhaoqiang <douhaoqiang@126.com>
2017-07-26 10:34:32 +0800

    添加依赖

--- paylibrary/src/main/java/com/bjgoodwill/paylibrary/entity/PayTypeEntity.java
+++ paylibrary/src/main/java/com/bjgoodwill/paylibrary/entity/PayTypeEntity.java
@@ -1,7 +1,7 @@
 package com.bjgoodwill.paylibrary.entity;
 
 
-import bjgoodwill.baselibrary.entity.BaseEntity;
+import com.dhq.baselibrary.entity.BaseEntity;
 
 /**
  * Created by dongpeng on 2016/12/2.
