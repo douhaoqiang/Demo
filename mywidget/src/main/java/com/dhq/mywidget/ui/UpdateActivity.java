dec14772b74861dc5a1685659412eba057afcb65
douhaoqiang <douhaoqiang@126.com>
2019-01-25 17:43:59 +0800

    调试更新

--- mywidget/src/main/java/com/dhq/mywidget/ui/UpdateActivity.java
+++ mywidget/src/main/java/com/dhq/mywidget/ui/UpdateActivity.java
@@ -20,7 +20,7 @@ import java.io.File;
  * DESC
  * Created by douhaoqiang on 2019/1/7.
  */
-public class UpdateActivity extends BaseActivity implements OnDownloadListener, OnButtonClickListener {
+public class UpdateActivity extends BaseActivity implements OnDownloadListener {
 
     private Button btn_update;
     private DownloadManager manager;
@@ -66,7 +66,7 @@ public class UpdateActivity extends BaseActivity implements OnDownloadListener, 
                 //输出错误日志
                 .setEnableLog(true)
                 //设置自定义的下载
-                .setHttpManager(new MyDownload())
+//                .setHttpManager(new MyDownload())
                 //下载完成自动跳动安装页面
                 .setJumpInstallPage(true)
                 //设置对话框背景图片 (图片规范参照demo中的示例图)
@@ -74,21 +74,19 @@ public class UpdateActivity extends BaseActivity implements OnDownloadListener, 
                 //设置按钮的颜色
                 //.setDialogButtonColor(Color.parseColor("#E743DA"))
                 //设置按钮的文字颜色
-                .setDialogButtonTextColor(Color.WHITE)
+//                .setDialogButtonTextColor(Color.WHITE)
                 //支持断点下载
                 .setBreakpointDownload(true)
                 //设置是否显示通知栏进度
                 .setShowNotification(true)
                 //设置强制更新
-                .setForcedUpgrade(true)
-//                .setHttpManager(new MyDownload())
-                //设置对话框按钮的点击监听
-                .setButtonClickListener(this)
+                .setForcedUpgrade(true);
                 //设置下载过程的监听
-                .setOnDownloadListener(this);
+//                .setOnDownloadListener(this);
 
         manager = DownloadManager.getInstance(this);
-        manager.setApkName("appupdate.apk")
+        manager
+//                .setApkName("appupdate.apk")
 //                .setApkUrl("https://raw.githubusercontent.com/azhon/AppUpdate/master/apk/appupdate.apk")
                 .setApkUrl("http://download.hscpro.com/cecm/iOS/HSCAssessmentV1.2.0-20181207.apk")
                 .setSmallIcon(R.mipmap.ic_launcher)
@@ -99,11 +97,6 @@ public class UpdateActivity extends BaseActivity implements OnDownloadListener, 
                 .setAuthorities(getPackageName())
                 .setApkDescription("1.支持断点下载\n2.支持Android N\n3.支持Android O\n4.支持自定义下载过程\n5.支持 设备>=Android M 动态权限的申请\n6.支持通知栏进度条展示(或者自定义显示进度)")
                 .download();
-    }
-
-    @Override
-    public void onButtonClick(int id) {
-
     }
 
     @Override
