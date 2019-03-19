b89444751926c4388bc5683b09046c4d4018cda2
douhaoqiang <douhaoqiang@126.com>
2019-03-19 17:45:41 +0800

    优化弹框工具类

--- mywidget/src/main/java/com/dhq/mywidget/dialog/DialogUtils.java
+++ mywidget/src/main/java/com/dhq/mywidget/dialog/DialogUtils.java
@@ -1,25 +1,63 @@
 package com.dhq.mywidget.dialog;
 
 /**
- * DESC
+ * DESC 弹框工具类
  * Created by douhaoqiang on 2019/3/18.
  */
 public class DialogUtils {
 
+    private Builder mBuilder;
 
+    private DialogUtils(Builder builder) {
+        this.mBuilder=builder;
+    }
 
-
+    /**
+     * 弹框显示
+     */
     public void show(){
+
+    }
+
+    /**
+     * 弹框消失
+     */
+    public void dismiss(){
 
     }
 
 
     public static class Builder{
 
+        /*
+        点击外面是否可以消失
+         */
+        boolean isDismissOutTouch=true;
+
+        /*
+        是否显示半透明背景
+         */
+        boolean isShadowBg=true;
+
+        /**
+         * 是否可以点击周围消失
+         * @param dismissOutTouch
+         */
+        public void setDismissOutTouch(boolean dismissOutTouch) {
+            isDismissOutTouch = dismissOutTouch;
+        }
+
+        /**
+         * 是否显示半透明背景
+         * @param shadowBg
+         */
+        public void setShadowBg(boolean shadowBg) {
+            isShadowBg = shadowBg;
+        }
 
         public DialogUtils build(){
 
-            return new DialogUtils();
+            return new DialogUtils(this);
         }
 
     }
