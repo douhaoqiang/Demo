8f6822328abc10e49502cfe623fb180671b1745c
douhaoqiang <douhaoqiang@126.com>
2019-03-19 16:39:53 +0800

    添加dialog工具类

---
+++ mywidget/src/main/java/com/dhq/mywidget/dialog/DialogUtils.java
@@ -0,0 +1,28 @@
+package com.dhq.mywidget.dialog;
+
+/**
+ * DESC
+ * Created by douhaoqiang on 2019/3/18.
+ */
+public class DialogUtils {
+
+
+
+
+    public void show(){
+
+    }
+
+
+    public static class Builder{
+
+
+        public DialogUtils build(){
+
+            return new DialogUtils();
+        }
+
+    }
+
+
+}
